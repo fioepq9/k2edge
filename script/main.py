db33bd515c9a27d8217c6f8ff88b05ba7d46d257
fino <liujinfeng@shoplazza.com>
2023-02-21 11:12:07 +0800

    update script

--- script/main.py
+++ script/main.py
@@ -1,5 +1,6 @@
 import typer
 from rich import print
+import subprocess
 
 def Alert():
     print("\n:broken_heart: [bold red]Alert! The script run failed.[/bold red]\n")
@@ -7,58 +8,44 @@ def Alert():
 def Ok():
     print("\n:beers: [bold green]Finish! The script run success.[/bold green]\n")
 
+def RunCommand(cmd: str):
+    r: subprocess.CompletedProcess = subprocess.run(cmd, shell=True)
+    # check result
+    if r.returncode != 0:
+        Alert()
+    else:
+        Ok()
+
 
 app = typer.Typer()
 
 @app.command()
 def updateAPI(name: str = "worker-api"):
-    # process
-    import subprocess
     if name == "worker-api":
-        r: subprocess.CompletedProcess = subprocess.run("goctl api go -api ./api/worker.api -dir ./worker -style goZero --home ./template", shell=True)
+        RunCommand("goctl api go -api ./api/worker.api -dir ./worker -style goZero --home ./template")
     elif name == "master-api":
-        r: subprocess.CompletedProcess = subprocess.run("goctl api go -api ./api/master.api -dir ./master -style goZero --home ./template", shell=True)
+        RunCommand("goctl api go -api ./api/master.api -dir ./master -style goZero --home ./template")
     else:
         print("unsupported arguments:", name)
 
-    # check result
-    if r.returncode != 0:
-        Alert()
-    else:
-        Ok()
 
 @app.command()
 def updateSwagger(name: str = "worker-api", port: int = 8080):
-    # process
-    import subprocess
     if name == "worker-api":
-        r: subprocess.CompletedProcess = subprocess.run(f"goctl api plugin -plugin goctl-swagger='swagger -filename worker.json --host 127.0.0.1:{port}' -api ./api/worker.api -dir ./worker/swagger", shell=True)
+        RunCommand(f"goctl api plugin -plugin goctl-swagger='swagger -filename worker.json --host 127.0.0.1:{port}' -api ./api/worker.api -dir ./worker/swagger")
     elif name == "master-api":
-        r: subprocess.CompletedProcess = subprocess.run(f"goctl api plugin -plugin goctl-swagger='swagger -filename master.json --host 127.0.0.1:{port}' -api ./api/master.api -dir ./master/swagger", shell=True)
+        RunCommand(f"goctl api plugin -plugin goctl-swagger='swagger -filename master.json --host 127.0.0.1:{port}' -api ./api/master.api -dir ./master/swagger")
     else:
         print("unsupported arguments:", name)
-    # check result
-    if r.returncode != 0:
-        Alert()
-    else:
-        Ok()
 
 @app.command()
 def runSwagger(name: str = "worker-api", port: int = 8083):
-    # process
-    import subprocess
     if name == "worker-api":
-        r: subprocess.CompletedProcess = subprocess.run(f"docker run --rm -p {port}:8080 -e SWAGGER_JSON=/app/worker.json -v $PWD/worker/swagger:/app swaggerapi/swagger-ui", shell=True)
+        RunCommand(f"docker run --rm -p {port}:8080 -e SWAGGER_JSON=/app/worker.json -v $PWD/worker/swagger:/app swaggerapi/swagger-ui")
     elif name == "master-api":
-        r: subprocess.CompletedProcess = subprocess.run(f"docker run --rm -p {port}:8080 -e SWAGGER_JSON=/app/master.json -v $PWD/master/swagger:/app swaggerapi/swagger-ui", shell=True)
+        RunCommand(f"docker run --rm -p {port}:8080 -e SWAGGER_JSON=/app/master.json -v $PWD/master/swagger:/app swaggerapi/swagger-ui")
     else:
         print("unsupported arguments:", name)
-    # check result
-    if r.returncode != 0:
-        Alert()
-    else:
-        Ok()
-
 
 @app.command()
 def version(name: str):
