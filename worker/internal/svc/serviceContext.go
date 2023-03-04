e742c9238ef9d36b88afd8fbb0f90714ef537e88
fino <liujinfeng@shoplazza.com>
2023-03-04 16:43:32 +0800

    polsih

--- worker/internal/svc/serviceContext.go
+++ worker/internal/svc/serviceContext.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"k2edge/worker/internal/config"
 	"k2edge/worker/internal/middleware"
+	"sync"
 	"time"
 
 	"github.com/zeromicro/go-zero/rest"
@@ -11,6 +12,11 @@ import (
 	"github.com/docker/docker/client"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+var (
+	C    *ServiceContext
+	once sync.Once
 )
 
 type ServiceContext struct {
@@ -21,26 +27,31 @@ type ServiceContext struct {
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
-	dockerCli, err := client.NewClientWithOpts(client.FromEnv)
-	if err != nil {
-		panic(err)
+	if C == nil {
+		once.Do(func() {
+			dockerCli, err := client.NewClientWithOpts(client.FromEnv)
+			if err != nil {
+				panic(err)
+			}
+			_, err = dockerCli.Ping(context.TODO())
+			if err != nil {
+				panic(err)
+			}
+			config := clientv3.Config{
+				Endpoints:   c.Etcd.Endpoints,
+				DialTimeout: time.Duration(c.Etcd.DialTimeout) * time.Second,
+			}
+			etcd, err := clientv3.New(config)
+			if err != nil {
+				panic(err)
+			}
+			C = &ServiceContext{
+				Config:         c,
+				AuthMiddleware: middleware.NewAuthMiddleware().Handle,
+				DockerClient:   dockerCli,
+				Etcd:           etcd,
+			}
+		})
 	}
-	_, err = dockerCli.Ping(context.TODO())
-	if err != nil {
-		panic(err)
-	}
-	config := clientv3.Config{
-		Endpoints:   c.Etcd.Endpoints,
-		DialTimeout: time.Duration(c.Etcd.DialTimeout) * time.Second,
-	}
-	etcd, err := clientv3.New(config)
-	if err != nil {
-		panic(err)
-	}
-	return &ServiceContext{
-		Config:         c,
-		AuthMiddleware: middleware.NewAuthMiddleware().Handle,
-		DockerClient:   dockerCli,
-		Etcd:           etcd,
-	}
+	return C
 }
